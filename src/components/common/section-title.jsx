0c1622428db4cbfe6cb1c703b55d9b3b7e01e561
Ahmet Tekin <aktekintr@gmail.com>
2024-08-22 13:46:33 +0300

    section-title

--- src/components/common/section-title.jsx
+++ src/components/common/section-title.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./section-title.scss";
+import { Container } from "react-bootstrap";
 
 const SectionTitle = ({ children }) => {
-	return <h2 className="section-title">{children}</h2>;
+  return (
+    <div className="section-title-wrapper">
+      <Container>
+        <h2 className="section-title">{children}</h2>
+      </Container>
+    </div>
+  );
 };
 
 export default SectionTitle;
