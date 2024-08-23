152206e7b3ae4ab8e253dcbfb5a4af4657d070d2
Ahmet Tekin <aktekintr@gmail.com>
2024-08-23 12:35:14 +0300

    privacy-policy

---
+++ src/app/(menu)/privacy-policy/page.jsx
@@ -0,0 +1,13 @@
+import SectionTitle from "@/components/common/section-title";
+import PrivacyPolicy from "@/components/menu/privacy-policy/PrivacyPolicy";
+
+const PrivacyPolicyPage = () => {
+  return (
+    <>
+      <SectionTitle>PRIVACY POLICY</SectionTitle>
+      <PrivacyPolicy/>
+    </>
+  );
+};
+
+export default PrivacyPolicyPage;
